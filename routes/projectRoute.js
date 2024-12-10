cd2a4ea03879723614a85a3e384fa82067d59694
Affandi99-bit <affandi.blck@gmail.com>
2024-12-10 14:30:35 +0700

    online trial

--- routes/projectRoute.js
+++ routes/projectRoute.js
@@ -7,9 +7,25 @@ import {
 } from "../controller/projectController.js";
 
 const route = express.Router();
+
 route.get("/getallprojects", (req, res, next) => {
   console.log("GET /getallprojects route hit");
   fetch(req, res);
+});
+
+route.get("/search", async (req, res, next) => {
+  const { pic, client, pm } = req.query;
+  const query = {};
+  if (pic) query.pic = { $regex: pic, $options: "i" };
+  if (client) query.client = { $regex: client, $options: "i" };
+  if (pm) query.pm = { $regex: pm, $options: "i" };
+
+  try {
+    const results = await Project.find(query);
+    res.json(results);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 route.post("/create", (req, res, next) => {
