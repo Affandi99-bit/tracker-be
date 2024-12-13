46182d00c6c9343d18cd707df10a8ed86ce65c93
Affandi99-bit <affandi.blck@gmail.com>
2024-12-13 12:30:53 +0700

    remove socket.io

--- index.js
+++ index.js
@@ -6,21 +6,11 @@ import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cors from "cors";
 import route from "./routes/projectRoute.js";
-// import http from "node:http";
-// import { Server } from "socket.io";
 
 const app = express();
-// const server = http.createServer(app);
 
 const PORT = process.env.PORT || 5001;
 const MONGOURL = process.env.MONGO_URL;
-
-// const io = new Server(server, {
-//   cors: {
-//     origin: ["https://blackmanager.netlify.app"],
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//   },
-// });
 
 // Middleware
 app.use(cors({ origin: "https://blackmanager.netlify.app" }));
@@ -29,34 +19,11 @@ app.use(bodyParser.json());
 // Routes
 app.use("/api/report", route);
 
-// Real-time Updates
-// io.on("connection", (socket) => {
-//   console.log("Client connected:", socket.id);
-
-//   socket.on("createData", (newData) => {
-//     io.emit("dataCreated", newData);
-//     console.log("this post updated with socket.io");
-//   });
-
-//   socket.on("updateData", (updatedData) => {
-//     io.emit("dataUpdated", updatedData);
-//   });
-
-//   socket.on("deleteData", (deletedId) => {
-//     io.emit("dataDeleted", deletedId);
-//   });
-
-//   socket.on("disconnect", () => {
-//     console.log("Client disconnected:", socket.id);
-//   });
-// });
-
 // Database Connection
 mongoose
   .connect(MONGOURL)
   .then(() => {
     console.log("Database connected successfully.");
-    // server.listen(PORT, "0.0.0.0", () => {
     app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server running`);
     });
